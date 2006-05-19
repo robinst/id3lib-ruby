bf4f9ebb22574e3589ff26ffe477640da0e2d93f
Robin Stocker <robin@nibor.org>
2006-05-19 16:33:41 +0000

    * Fixed extconf.rb to print a message and abort if id3lib can't be found. * Removed "description" meta tag of web page.
    git-svn-id: http://id3lib-ruby.rubyforge.org/svn@23 07407819-590d-0410-b965-85670ce66263

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,6 +1,7 @@
-
 require 'mkmf'
 
-have_header('id3.h')
-have_library('id3', 'ID3Tag_New')
-create_makefile('id3lib_api')
+if have_header('id3.h') and have_library('id3', 'ID3Tag_New')
+  create_makefile('id3lib_api')
+else
+  message "You must have id3lib installed in order to use id3lib-ruby!\n"
+end
