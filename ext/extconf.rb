6e4d5b8620c50ad86caf5034c750478a1dd06d2d
Robin Stocker <robin@nibor.org>
2006-06-05 00:16:12 +0000

    * Corrected order of checks in ext/extconf.rb, should be right this time. * Re-release of 0.3.1
    git-svn-id: http://id3lib-ruby.rubyforge.org/svn@32 07407819-590d-0410-b965-85670ce66263

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -5,16 +5,16 @@ def error msg
   abort
 end
 
-unless have_header('id3.h') and have_library('id3', 'ID3Tag_New')
-  error "You must have id3lib installed in order to use id3lib-ruby."
-end
-
 unless have_library('stdc++')
   error "You must have libstdc++ installed."
 end
 
 unless have_library('z')
   error "You must have zlib installed."
+end
+
+unless have_header('id3.h') and have_library('id3', 'ID3Tag_New')
+  error "You must have id3lib installed in order to use id3lib-ruby."
 end
 
 create_makefile('id3lib_api')
