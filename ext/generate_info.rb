bf38b712f3c64f1014af851f7b32a8b71403c28a
Robin Stocker <robin@nibor.org>
2006-12-03 22:36:48 +0000

    * ext/generate_info.rb: obtain allowed fields from id3lib source
    git-svn-id: http://id3lib-ruby.rubyforge.org/svn/trunk@98 07407819-590d-0410-b965-85670ce66263

--- ext/generate_info.rb
+++ ext/generate_info.rb
@@ -1,71 +1,75 @@
+#!/usr/bin/ruby
+
 require 'enumerator'
+require 'pathname'
 
-file = ARGV.first || '/usr/local/include/id3/globals.h'
 
-data = IO.read(file)
+id3lib_dir = Pathname.new(ARGV.first || '/home/robin/tmp/id3lib-3.8.3')
 
-fields = []
-frames = []
-genres = []
+globals_file = File.read(id3lib_dir + 'include/id3/globals.h')
+field_file   = File.read(id3lib_dir + 'src/field.cpp')
 
-data.scan(/ID3_ENUM\((\w+)\)\s+\{\s+(.+?)\s+\}/m) do |name, enum|
+
+field_groups   = {}
+allowed_fields = {}
+
+field_file.scan(/ID3_FieldDef (\w+)[^\{]+\{(.+?)\};/m) do |group, body|
+  fields = []
+  body.scan(/\{\W+(\w+)/) do |field_name, _|
+    next if field_name == "ID3FN_NOFIELD"
+    fields << field_name
+  end
+  fields.uniq!
+  fields.map!{ |f| f[/ID3FN_(.*)/, 1].downcase.to_sym }
+  field_groups[group] = fields
+end
+
+field_file.scan(/ID3_FrameDef ID3_FrameDefs[^\{]+\{(.+?)\};/m) do |body, _|
+  body.each_line do |line|
+    values = line.split(/\s*,\s*/)
+    next unless values.size > 1
+    frame = values[2].delete('"')
+    group = values[5]
+    next if frame.empty?
+    allowed_fields[frame.to_sym] = field_groups[group]
+  end
+end
+
+
+field_info = []
+frame_info = []
+genre_info = []
+
+globals_file.scan(/ID3_ENUM\((\w+)\)\s+\{\s+(.+?)\s+\}/m) do |name, enum|
   case name
   when 'ID3_FieldID'
     id = 0
     enum.scan(/([^\s,]+)(?: = (\d+),|,)\s*\/\*\*< ([^*]+)\s+\*\//m) do |field, newid, description|
       id = newid.to_i if newid
-      field.sub!('ID3FN_', '')
-      field.downcase!
-      fields << [id, field.to_sym, description]
+
+      field = field[/ID3FN_(.*)/, 1].downcase.to_sym
+      field_info << [id, field, description]
+
       id += 1
     end
   when 'ID3_FrameID'
     id = 0
     enum.scan(/\/\* (\S+) \*\/ [^\s,]+(?: = (\d+),|,)\s*\/\*\*< ([^*]+)\s+\*\//m) do |frame, newid, description|
       id = newid.to_i if newid
-      if frame == '????'
-        frame = :____
-      else
-        frame = frame.to_sym
-      end
-      possible_fields =
-      case frame
-      when :____ : []
-      when :AENC : [:owner, :data]
-      when :APIC : [:textenc, :mimetype, :picturetype, :description, :data]
-      when :ASPI, :COMR, :EQUA, :ETCO, :LINK,
-        :MCDI, :MLLT, :OWNE, :POSS, :RBUF,
-        :RVA2, :RVAD, :RVRB, :SEEK
-        [:data]
-      when :COMM : [:textenc, :language, :description, :text]
-      when :ENCR : [:owner, :id, :data]
-      when :EQU2 : [:id, :text]
-      when :GEOB : [:textenc, :mimetype, :filename, :description, :data]
-      when :GRID : [:owner, :id, :data]
-      when :IPLS : [:textenc, :text]
-      when :PRIV : [:owner, :data]
-      when :PCNT : [:counter]
-      when :POPM : [:email, :rating, :counter]
-      when :SIGN : [:id, :data]
-      when :SYLT : [:textenc, :language, :timestampformat, :contenttype, :description, :data]
-      when :SYTC : [:timestampformat, :data]
-      when :UFID : [:owner, :data]
-      when :USER : [:textenc, :language, :text]
-      when :USLT : [:textenc, :language, :description, :text]
-      when :WCOM, :WCOP, :WOAF, :WOAR, :WOAS, :WORS, :WPAY, :WPUB : [:text]
-      when :WXXX : [:textenc, :description, :url]
-      else [:textenc, :text]
-      end
-      frames << [id, frame, description, possible_fields]
+      frame = (frame == '????') ? :____ : frame.to_sym
+
+      fields = allowed_fields[frame] || []
+
+      frame_info << [id, frame, description, fields]
       id += 1
     end
   end
 end
 
-data.scan(/(ID3_v1_genre_description).+?\{(.+?)\}/m) do |name, list|
+globals_file.scan(/(ID3_v1_genre_description).+?\{(.+?)\}/m) do |name, list|
   id = 0
-  list.scan(/"(.+?)"/) do |genre|
-    genres << genre.first
+  list.scan(/"([^"]+)"/) do |genre|
+    genre_info << genre.first
     id += 1
   end
 end
@@ -76,7 +80,7 @@ def indent level, text
 end
 
 indent 4, "Frames = ["
-frames.each do |f|
+frame_info.each do |f|
   comment = case f[1]
   when :____ : "# Special frames"
   when :TALB : "# Text information frames"
@@ -89,25 +93,25 @@ end
 indent 4, "]"
 
 indent 4, "FramesByID = {"
-frames.each do |f|
+frame_info.each do |f|
   indent 6, f[1].inspect + " => Frames[" + f[0].to_s + "],"
 end
 indent 4, "}"
 
 indent 4, "Fields = ["
-fields.each do |f|
+field_info.each do |f|
   indent 6, f.inspect + ","
 end
 indent 4, "]"
 
 indent 4, "FieldsByID = {"
-fields.each do |f|
+field_info.each do |f|
   indent 6, f[1].inspect.ljust(16) + " => Fields[" + f[0].to_s + "],"
 end
 indent 4, "}"
 
 indent 4, "Genres = ["
-genres.each_slice(4) do |gs|
+genre_info.each_slice(4) do |gs|
   indent 6, "# Winamp extensions" if gs.first == "Folk"
   indent 6, gs.map{ |g| g.inspect }.join(", ") + ","
 end
