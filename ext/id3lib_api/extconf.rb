ef896189dbdb540a81b843d79f01287df9b77d7d
Robin Stocker <robin@nibor.org>
2008-12-18 21:40:42 +0100

    Optional check for iconv in extconf.rb
    Without -liconv, linking on Windows doesn't work somehow.

--- ext/id3lib_api/extconf.rb
+++ ext/id3lib_api/extconf.rb
@@ -13,6 +13,9 @@ unless have_library('z')
   error "You must have zlib installed."
 end
 
+# This is only necessary for linking on Windows (don't ask me why).
+have_library('iconv')
+
 unless have_header('id3.h') and have_library('id3')
   error "You must have id3lib installed in order to use id3lib-ruby."
 end
