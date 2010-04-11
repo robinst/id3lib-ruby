9956bd84319ea59d26be205a7b3601c47ca7c631
Robin Stocker <robin@nibor.org>
2010-04-11 22:46:37 +0200

    Clean up code and style

--- test/data/create.cpp
+++ test/data/create.cpp
@@ -4,15 +4,24 @@
 
 using namespace std;
 
-void AddTextFrame( ID3_Tag & tag, ID3_FrameID frameid, char * text ) {
+void AddTextFrame(ID3_Tag & tag, ID3_FrameID frameid, char * text) {
 	ID3_Frame frame(frameid);
 	frame.Field(ID3FN_TEXT).Set(text);
 	tag.AddFrame(frame);
 }
 
-int main( int argc, char ** argv ) {
-	ID3_Tag tag;	
-	tag.Link( argv[1] );
+int main(int argc, char ** argv) {
+	const char * filename;
+	ID3_Tag tag;
+
+	if (argc != 2) {
+		cout << "usage: " << argv[0] << " output.mp3" << endl;
+		return 1;
+	}
+
+	filename = argv[1];
+
+	tag.Link(filename);
 
 	AddTextFrame(tag, ID3FID_TITLE, "Dummy Title");
 	AddTextFrame(tag, ID3FID_LEADARTIST, "Dummy Artist");
